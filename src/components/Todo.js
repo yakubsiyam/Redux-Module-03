458ce188aa76ae26eef1c50d85c0a30e4a3cab84
yakubsiyam <itsyakubsiyam@gmail.com>
2023-02-28 22:09:42 +0600

    Color toggling and deleting feature in todoList

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import cancelImage from "../assets/images/cancel.png";
-import { toggled } from "../redux/todos/actions";
-import { colorSelected } from "../redux/todos/actions";
+import { toggled, colorSelected, deleted } from "../redux/todos/actions";
 
 const Todo = ({todo}) => {
   const dispatch = useDispatch();
@@ -12,8 +11,12 @@ const Todo = ({todo}) => {
     dispatch(toggled(todoId))
   }
 
-  const handleColorChange = () => {
+  const handleColorChange = (todoId, color) => {
     dispatch(colorSelected(todoId, color));
+  }
+
+  const handleDelete = (todoId) => {
+    dispatch(deleted(todoId));
   }
   return (
     <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
@@ -34,16 +37,17 @@ const Todo = ({todo}) => {
         {text}
       </div>
 
-      <div className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-green-500 border-green-500 ${color === 'green' && 'bg-green-500'}`}></div>
+      <div className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-green-500 border-green-500 ${color === 'green' && 'bg-green-500'}`} onClick={() => handleColorChange(id, "green")}></div>
 
-      <div className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-yellow-500 border-yellow-500  ${color === 'yellow' && 'bg-yellow-500'}`}></div>
+      <div className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-yellow-500 border-yellow-500  ${color === 'yellow' && 'bg-yellow-500'}`} onClick={() => handleColorChange(id, "yellow")}></div>
 
-      <div className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-red-500 border-red-500 ${color === 'red' && 'bg-red-500'}`}></div>
+      <div className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-red-500 border-red-500 ${color === 'red' && 'bg-red-500'}`} onClick={() => handleColorChange(id, "red")}></div>
 
       <img
         src={cancelImage}
         className="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
         alt="Cancel"
+        onClick={() => handleDelete(id)}
       />
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,6 @@ import Todo from "./Todo";
 
 const TodoList = () => {
   const todos = useSelector((state) => state?.todos);
-  console.log(todos);
 
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
