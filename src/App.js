7d428b24b16d61ddbaceef6e267ac55340dcf77f
Siyam <itsyakubsiyam@gmail.com>
2023-02-28 09:59:18 +0600

    Setup redux project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <Navbar></Navbar>
+
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+        <Header></Header>
+
+        <hr className="mt-4" />
+
+        <TodoList></TodoList>
+
+        <hr className="mt-4" />
+
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = () => {
+  return (
+    <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+      <Todo></Todo>
+    </div>
+  );
+};
+
+export default TodoList;
